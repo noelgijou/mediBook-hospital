01bf68b11205e12f59f97e4b6493ff12639834b7
noelgijou <81475664+noelgijou@users.noreply.github.com>
2021-07-07 23:58:36 +0530

    patients home components added

--- mediBook-hospital/frontend/src/app/app-routing.module.ts
+++ mediBook-hospital/frontend/src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { BookappointComponent } from './bookappoint/bookappoint.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MyrecordsComponent } from './myrecords/myrecords.component';
+import { PatientprofileComponent } from './patientprofile/patientprofile.component';
+import { UsermenuComponent } from './usermenu/usermenu.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'usermenu',component:UsermenuComponent,
+children:[
+  {path:'',component:PatientprofileComponent},
+  {path:'appointments',component:AppointmentsComponent},
+  {path:'myrecords',component:MyrecordsComponent},
+  {path:'bookappoint',component:BookappointComponent}
+]},
+
 ];
 
 @NgModule({
