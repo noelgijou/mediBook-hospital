11b3d0afe7cb977df53bd5b94c9d5201affe387c
Hrishikesh Mohan <50671806+hkm7@users.noreply.github.com>
2021-07-30 16:46:09 +0530

    marco

--- mediBook-hospital/backend/app.js
+++ mediBook-hospital/backend/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+<<<<<<< Updated upstream
+=======
+const mongoose = require('./database/mongoose');
+
+
+>>>>>>> Stashed changes
 app.get('/', function(req,res){
     res.send("You're not supposed to be here!!")
 });
 
-app.listen(3000);
+app.listen(8080, ()=> console.log('Server connected- Express up and running, Database connected'));
---
+++ mediBook-hospital/backend/database/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb+srv://hrishi:BhbQd6P2sVFYSxQ@hkm7.js3cl.mongodb.net:27017/test',{useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=> console.log("Database connected"))
+    .catch((error)=> console.log(error));
+
+module.exports = mongoose;
